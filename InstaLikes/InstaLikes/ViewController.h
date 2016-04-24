f871b1cf55c52584771e7c558d1e52f22c8f071a
DENISA ORIANA TODEREAN <denisa.toderean@gmail.com>
2016-04-24 16:03:29 +0200

    Initial commit

---
+++ InstaLikes/InstaLikes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaLikes
+//
+//  Created by DENISA ORIANA TODEREAN on 24/04/16.
+//  Copyright © 2016 DENISA ORIANA TODEREAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstaLikes/InstaLikes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstaLikes
+//
+//  Created by DENISA ORIANA TODEREAN on 24/04/16.
+//  Copyright © 2016 DENISA ORIANA TODEREAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
